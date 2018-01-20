52c34488d215d0d8bf84d6e8bbe151b2b1430d2e
Matteo Meli <matteo.meli@gmail.com>
2018-01-20 14:17:59 +0000

    Finish type class exercises

--- src/main/scala/learnfp/typeclass/Eq.scala
+++ src/main/scala/learnfp/typeclass/Eq.scala
@@ -18,14 +18,22 @@ object EqOps {
 
 object EqInstances {
   implicit val intEqInstance = new Eq[Int] {
-    override def eq(lhs: Int, rhs: Int): Boolean = ???
+    override def eq(lhs: Int, rhs: Int): Boolean = lhs == rhs
   }
 
   implicit val stringEqInstance = new Eq[String] {
-    override def eq(lhs: String, rhs: String): Boolean = ???
+    override def eq(lhs: String, rhs: String): Boolean = lhs == rhs
   }
 
   implicit def listEqInstance[A](implicit eqt:Eq[A]) = new Eq[List[A]] {
-    override def eq(lhs: List[A], rhs: List[A]): Boolean = ???
+    override def eq(lhs: List[A], rhs: List[A]): Boolean = {
+      /*(lhs, rhs) match {
+        case (Nil, Nil) => true
+        case (Nil, _) => false
+        case (_, Nil) => false
+        case (x, y) => x.head == y.head && eq(x.tail, y.tail)
+      }*/
+      lhs.length == rhs.length && lhs.zip(rhs).forall { case (l, r) => eqt.eq(l, r) }
+    }
   }
 }
--- src/main/scala/learnfp/typeclass/Show.scala
+++ src/main/scala/learnfp/typeclass/Show.scala
@@ -12,14 +12,24 @@ object Printer {
 
 object ShowInstances {
   implicit val intInstance:Show[Int] = new Show[Int] {
-    override def show(x: Int): String = ???
+    override def show(x: Int): String = f"$x"
   }
 
   implicit val doubleInstance:Show[Double] = new Show[Double] {
-    override def show(x: Double): String = ???
+    override def show(x: Double): String = f"$x"
   }
 
   implicit def listInstance[T](implicit xShow:Show[T]):Show[List[T]] = new Show[List[T]] {
-    override def show(xs:List[T]): String = ???
+    override def show(xs:List[T]): String = {
+      /*def loop(current: List[T], acc: String): String = {
+        current match {
+          case Nil => acc
+          case l :: Nil => acc + f"$l"
+          case h :: t  => loop(t, acc + f"$h, ")
+        }
+      }
+      s"[${loop(xs, "")}]"*/
+      xs.map(xShow.show(_)).mkString("[", ", ", "]")
+    }
   }
 }
